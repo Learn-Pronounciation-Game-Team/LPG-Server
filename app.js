08d9ca0d7f01cd565f08d39bffea5e0d6eb56dd3
jarooda <jaluwibowoaji@gmail.com>
2021-01-24 16:47:19 +0700

    add cors in app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
